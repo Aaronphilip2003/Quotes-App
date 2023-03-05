4def4f9cabbf3b6f2ee5ab1ee7d6b157c59a645b
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-03-05 23:12:38 +0530

    added author

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,14 @@ import quotes_img from "../images/quotes_img.webp"
 export default function Card() {
 
     const [quote, setQuote] = useState("");
+    const [author,setAuthor] = useState("");
 
     const getQuote = () => {
         axios
             .get("https://api.quotable.io/random")
             .then((res) => {
                 setQuote(res.data.content)
+                setAuthor(res.data.author)
             })
             .catch((err) => {
                 console.log(err);
@@ -28,6 +30,8 @@ export default function Card() {
                     <h5 class="text-gray-900 text-xl font-medium mb-2">Quotes ❤️</h5>
                     <p class="text-gray-700 text-base mb-4">
                         {quote ? <h1 className="font-bold italic">{quote}</h1> : null}
+                        ----------------------------------------------------------
+                        {author? <h2 className="font-bold italic">{author}</h2> : null}
                     </p>
                     <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getQuote}>New Quote</button>
                 </div>
