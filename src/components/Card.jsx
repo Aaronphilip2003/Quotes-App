7d0be4ad09debcb73c117389ec6f298ef8532db3
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-03-06 09:18:22 +0530

    removed mail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { React, useState } from "react";
 import quotes_img from "../images/quotes_img.webp";
-import emailjs from 'emailjs-com';
 
 export default function Card() {
   const [quote, setQuote] = useState("");
@@ -16,32 +15,6 @@ export default function Card() {
       })
       .catch((err) => {
         console.log(err);
-      });
-  };
-
-  const sendMail = () => {
-    // Prompt the user to enter the recipient's email
-    const toEmail = prompt("Enter the recipient's email address:");
-  
-    // If the user cancelled the prompt or didn't enter a value, return
-    if (!toEmail) {
-      return;
-    }
-  
-    // Set up the email parameters
-    const emailParams = {
-      to_email: toEmail,
-      from_email: 'your_email@example.com', // Replace with the user's email address
-      message: quote,
-      author: author
-    };
-  
-    // Send the email using emailJS
-    emailjs.send('service_fxwxvb6', 'template_ogzfqka', emailParams, 'fLdkQ4a8MGGCGFwkO')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
       });
   };
   
@@ -72,15 +45,6 @@ export default function Card() {
             onClick={getQuote}
           >
             New Quote
-          </button>
-          <br />
-          <br />
-          <button
-            type="button"
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-            onClick={sendMail}
-          >
-            Mail it to a loved one!
           </button>
         </div>
       </div>
